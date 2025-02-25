caf1d502a9f23aeeda9d92aef23b5a0e15c142cf
Kai Aldag <kaialdag@icloud.com>
2025-02-25 16:03:25 +0000

    update orchestrator URL

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,7 +4,7 @@ export * from './smartContractTypes'
 export * from './registry'
 export * from './typehashTypes'
 
-export const ORCHESTRATOR_URL = 'https://orchestrator.api.rhinestone.wtf'
+export const ORCHESTRATOR_URL = 'https://orchestrator.rhinestone.wtf'
 
 export const ORCHESTRATOR_PUBLIC_KEY: Address =
   '0x8a310b9085faF5d9464D84C3d9a7BE3b28c94531'
